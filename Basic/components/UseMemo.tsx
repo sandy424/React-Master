d7c619a9bdd61e249614ba4bf38e75b71d007eeb
sandy424 <sandy424@naver.com>
2026-02-23 14:01:13 +0900

    useCallback 사용법과 useEffect와의 차이점

--- Basic/components/UseMemo.tsx
+++ Basic/components/UseMemo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 const getAverage = (numbers: number[]) =>{
     console.log("평균값 계산 중...");
@@ -9,24 +9,35 @@ const getAverage = (numbers: number[]) =>{
 
 function UseMemo() {
 
+    // useCallback - 함수 자체를 반환
+    // 주로 렌더링 성능을 최적화해야 하는 상황에서 사용됨.
+    // 이 훅을 사용하면 만들어 놓았던 함수를 재사용할 수 있다.
+
     const [list, setList] = useState<number[]>([]);
     const [number, setNumber] = useState<string>("");
 
-    const onInsert = () => {
+    const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setNumber(e.target.value);
+    }, []);
+
+    const onInsert = useCallback(() => {
         // concat: Array 인스턴스의 concat 함수는 두 개 이상의 배열을 병합하는 데 사용된다.
         // 이 메서드는 기존 배열을 변경하지 않고, 새 배열을 반환한다.
         const newList = list.concat(parseInt(number));
         setList(newList);
         setNumber("");
-    };
+    }, [list, number]);
     
     const onReset = () => {
         setList([]);
     }
 
+    // useMemo은 특정 값이 변경될 때만 함수를 실행하도록 하는 Hook입니다.
+    const average = useMemo(() => getAverage(list), [list]);
+
     return(
         <div>
-            <input type="text" value={number} onChange={(e) => setNumber(e.target.value)} />
+            <input type="text" value={number} onChange={onChange} />
             <button onClick={onInsert}>등록</button>
             <button onClick={onReset}>초기화</button>
 
@@ -37,7 +48,7 @@ function UseMemo() {
             </ul>
 
             <div>
-                <b>평균 값: {getAverage(list)}</b>
+                <b>평균 값: {average}</b>
             </div>
         </div>
     )
