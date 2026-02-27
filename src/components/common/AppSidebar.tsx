fadd4e84730186253189224566c490e585943670
sandy424 <sandy424@naver.com>
2026-02-28 00:02:59 +0900

    [add] sidebar 컴포넌트

--- src/components/common/AppSidebar.tsx
+++ src/components/common/AppSidebar.tsx
@@ -6,7 +6,7 @@ function AppSidebar() {
     return(
         <aside className="min-w-60 w-60 flex flex-col gap-6">
                   <div className="flex items-center gap-2">
-                    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">카테고리</h4>
+                    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight top-5">카테고리</h4>
                     <ChevronDown className="mt-1"/>
                   </div>
                   <div className="w-full flex flex-col gap-2">
