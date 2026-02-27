07ef964a78cda0642653c5ac288de17021ca5262
sandy424 <sandy424@naver.com>
2026-02-27 21:24:12 +0900

    페이지 라우팅

--- Basic/src/App.tsx
+++ Basic/src/App.tsx
@@ -1,11 +1,9 @@
-import UseRef from '../components/UseRef'
+
 
 function App() {
 
   return (
-    <div>
-      <UseRef />
-    </div>
+    <div></div>
   )
 }
 
--- Basic/src/main.tsx
+++ Basic/src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router';
+
 import App from './App.tsx'
+import SignIn from './pages/SignIn.tsx';
+import SignUp from './pages/SignUp.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}></Route>
+        <Route path='/sign-up' element={<SignUp/>}></Route>
+        <Route path='/sign-in' element={<SignIn/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
