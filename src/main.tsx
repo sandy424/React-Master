cb7ca36846d8cb78cb48dc5654b85fe95f0abdf4
sandy424 <sandy424@naver.com>
2026-03-05 17:22:42 +0900

    페이징 라우팅으로 전역 레이아웃 관리

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import './index.css';
+
+
+import App from './App.tsx';
+import SignIn from './pages/SignIn.tsx';
+import SignUp from './pages/SignUp.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}></Route>
+        <Route path='sign-in' element={<SignIn/>}></Route>
+        <Route path='sign-up' element={<SignUp/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,6 @@
+function SignIn() {
+    return(
+        <div></div>
+    )
+}
+export default SignIn;
